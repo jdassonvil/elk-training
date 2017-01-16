a0cc16e95e06639fcc2d8ebbc7debb8fe1019dfc
Jerome Dassonville <jdassonv@cisco.com>
2017-01-16 17:26:23 +0100

    Update imdb dataset
    Signed-off-by: Jerome Dassonville <jdassonv@cisco.com>

--- datasets/imdb/csvToJson.py
+++ datasets/imdb/csvToJson.py
@@ -7,9 +7,9 @@ class Movie:
     def __init__(self, title, director, duration, country, rating):
         self.title = title
         self.director = director
-        self.duration = duration
+        self.duration = None if not duration else int(duration)
         self.country = country
-        self.rating = rating
+        self.rating = None if not rating else float(rating)
 
     def toString(self):
         return self.title + '|' + self.director + '|' + self.country + '|' + self.rating
@@ -20,8 +20,15 @@ class Movie:
 with open('movie_metadata.csv', 'rb') as csvfile:
   csvfile = csv.reader(csvfile, delimiter=',')
   movies = []
+  jsonFile = open('movie_metadata.json', 'w')
+  bulkFile = open('movie_metadata.bulk', 'w')
+
   for row in csvfile:
       movie = Movie(row[11], row[1], row[3], row[20], row[25])
       movies.append(movie.__dict__)
+      bulkFile.write('{"create" : { "_index" : "movies", "_type" : "movie"}}\n')
+      bulkFile.write(movie.toJSON() + '\n')
 
-  print json.dumps(movies, sort_keys=False, indent=4, separators=(',', ': '))
+  bulkFile.close()
+  jsonFile.write(json.dumps(movies, sort_keys=False, indent=4, separators=(',', ': ')))
+  jsonFile.close()
