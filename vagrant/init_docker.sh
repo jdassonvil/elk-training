09dcd91237175d8882276c63d2a619af647b81c0
Jerome Dassonville <jdassonv@cisco.com>
2017-01-20 15:21:31 +0100

    Trashing old files
    Signed-off-by: Jerome Dassonville <jdassonv@cisco.com>

--- vagrant/init_docker.sh
+++
@@ -1,7 +0,0 @@
-yum update
-curl -fsSL https://get.docker.com/ | sh
-yum install docker-engine
-systemctl enable docker.service
-systemctl start docker
-curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
