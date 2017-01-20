e668af236829a53aad2a058c9d0411ac2e72edfa
Jerome Dassonville <jdassonv@cisco.com>
2017-01-20 19:13:49 +0100

    Update instance terraform configuration
    Signed-off-by: Jerome Dassonville <jdassonv@cisco.com>

--- env/terraform/instances.tf
+++ env/terraform/instances.tf
@@ -49,9 +49,6 @@ resource "openstack_compute_instance_v2" "elk-instance" {
      "sudo sh bootstrap-salt.sh",
      "sudo salt-call state.apply",
      "sudo usermod -G docker cloud-user",
-     "sudo curl -L \"https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
-     "sudo chmod +x /usr/local/bin/docker-compose",
-     "sudo docker-compose up -d"
    ]                                                                                                                   
   }                                                                                                                     
 }
