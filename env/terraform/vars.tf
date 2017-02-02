469ac4304ba1ce3a8b3a772dd9866a2aeb238e04
Jerome Dassonville <jdassonv@cisco.com>
2017-02-02 12:58:34 +0100

    Add vars for authentication
    Signed-off-by: Jerome Dassonville <jdassonv@cisco.com>

---
+++ env/terraform/vars.tf
@@ -0,0 +1,19 @@
+variable "tenant_name" {
+  type = "string"
+  description = "targeted tenant for openstack api"
+}
+
+variable "auth_url" {
+  type = "string"
+  description = "authentication endpoint of openstack api"
+}
+
+variable "user_name" {
+  type = "string"
+  description = "username credential for openstack api"
+}
+
+variable "password" {
+  type = "string"
+  description = "password credential for openstack api"
+}
